cad6cfb66989ce311b482b896f07531a0286ca2b
hesahamboshama96 <Heshamaboshama22@gmail.com>
2024-03-23 05:39:22 +0200

    updatePackages

--- tests/steps/checkoutStepTwoPage.steps.ts
+++ tests/steps/checkoutStepTwoPage.steps.ts
@@ -1,7 +1,6 @@
 import { CheckoutStepTwoPage } from '../../tests/pages/CheckoutStepTwoPage'
 import { ICustomWorld } from '../../support/custom-world'
 import { When } from '@cucumber/cucumber'
-import { scroll } from '../../support/helpers'
 
 When('Scrol down to the buttom then click on finish button', async function (this: ICustomWorld) {
   const checkoutStepTwoPage = new CheckoutStepTwoPage(this.page!)
