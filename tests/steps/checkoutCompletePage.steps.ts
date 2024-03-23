1ea957833b498a09cdf8f874de36629222bc18d3
hesahamboshama96 <Heshamaboshama22@gmail.com>
2024-03-23 06:08:49 +0200

    update

--- tests/steps/checkoutCompletePage.steps.ts
+++ tests/steps/checkoutCompletePage.steps.ts
@@ -1,10 +1,11 @@
 import { CheckoutCompletePage } from '../../tests/pages/CheckoutCompletePage'
 import { ICustomWorld } from '../../support/custom-world'
 import { Then } from '@cucumber/cucumber'
-import { expect } from '@playwright/test'
+import { expect } from 'playwright/test'
 
 Then('Verify that the order completion message appears correctly', async function (this: ICustomWorld) {
   const checkoutCompletePage = new CheckoutCompletePage(this.page!)
   expect(checkoutCompletePage.returnCheckoutCompleteContainer()).toContainText("Thank you for your order!")
   expect(checkoutCompletePage.returnCheckoutCompleteContainer()).toContainText("Your order has been dispatched, and will arrive just as fast as the pony can get there!")
 })
+
