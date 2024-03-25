1ce9348982648e16fb32a8d93e97bc0dbd7798e3
hesahamboshama96 <Heshamaboshama22@gmail.com>
2024-03-25 03:38:56 +0200

    addReporting

--- cucumber.js
+++ cucumber.js
@@ -1,16 +1,28 @@
+const getWorldParams = () => {
+  const params = {
+    foo: 'bar',
+  };
+
+  return params;
+};
+
 const config = {
     paths: ['tests/features/**/*.feature'],
     require: ['tests/steps/**/*.ts','support/**/*.ts' ],
     requireModule: ['ts-node/register'],
-    format: [
-      'summary',
-      'progress-bar',
-      '@cucumber/pretty-formatter'
-    ],
-    formatOptions: { snippetInterface: 'async-await' },
-    publishQuiet: true,
-  };
+  format: [
+    'json:reports/cucumber-report.json',
+    'html:reports/report.html',
+    'summary',
+    'progress-bar',
+  ],
+  formatOptions: { snippetInterface: 'async-await' },
+  worldParameters: getWorldParams(),
+  publishQuiet: true,
+};
 
+  config.format.push('@cucumber/pretty-formatter')
+  
 module.exports = {
-   default: config
- }
+  default: config
+}
